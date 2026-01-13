6aec01317cf11a88919af2061f49f341cacf31c7
hanaeHb <hanaebertili@gmail.com>
2026-01-13 20:38:56 +0100

    edit client

--- Repository/ClientRepository.py
+++ Repository/ClientRepository.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 from entity.Client import Client
 
 class ClientRepository:
@@ -8,27 +9,43 @@ class ClientRepository:
     # ----------------- CRUD -----------------
     def save(self, client: Client):
         """إضافة عميل جديد أو تحديث موجود"""
-        self.db_session.add(client)
-        self.db_session.commit()
-        return client
+        try:
+            self.db_session.add(client)
+            self.db_session.commit()
+            return client
+        except SQLAlchemyError as e:
+            self.db_session.rollback()
+            raise e
 
     def find_by_id(self, id: int):
         """جلب عميل حسب id"""
-        return self.db_session.query(Client).filter_by(id=id).first()
+        try:
+            return self.db_session.query(Client).filter_by(id=id).first()
+        except SQLAlchemyError as e:
+            self.db_session.rollback()
+            raise e
 
     def update(self, id: int, client_data: dict):
         """تحديث بيانات العميل حسب id"""
-        client = self.find_by_id(id)
-        if client:
-            for key, value in client_data.items():
-                setattr(client, key, value)
-            self.db_session.commit()
-        return client
+        try:
+            client = self.find_by_id(id)
+            if client:
+                for key, value in client_data.items():
+                    setattr(client, key, value)
+                self.db_session.commit()
+            return client
+        except SQLAlchemyError as e:
+            self.db_session.rollback()
+            raise e
 
     def delete(self, id: int):
         """حذف العميل حسب id"""
-        client = self.find_by_id(id)
-        if client:
-            self.db_session.delete(client)
-            self.db_session.commit()
-        return client
+        try:
+            client = self.find_by_id(id)
+            if client:
+                self.db_session.delete(client)
+                self.db_session.commit()
+            return client
+        except SQLAlchemyError as e:
+            self.db_session.rollback()
+            raise e
